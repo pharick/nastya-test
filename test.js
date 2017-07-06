51aa06334cda9631deb3cffb99ba2a5d843650cd
Артём <phar@localhost.localdomain>
2017-07-06 13:02:07 +0300

    Make test work

--- test.js
+++ test.js
@@ -2,23 +2,32 @@ $(document).ready(function() {
   var questions = [
     {
       questionText: 'Сколько раз в\u00A0день ты\u00A0говоришь \u00ABмяу\u00BB?',
-      answers: ['Ни\u00A0одного', 'Всего один', 'Максимум два-три', 'Около 100 раз']
+      answers: ['Ни\u00A0одного', 'Всего один', 'Максимум два-три', 'Около 100 раз'],
+      rightAnswer: 3
     },
 
     {
       questionText: 'Что ты\u00A0делаешь, когда видишь кота?',
-      answers: ['Прохожу мимо', 'Гавкаю', 'Кричу \u00ABкиса!\u00BB и\u00A0тискаю его', 'Начинаю чесаться']
+      answers: ['Прохожу мимо', 'Гавкаю', 'Кричу \u00ABкиса!\u00BB и\u00A0тискаю его', 'Начинаю чесаться'],
+      rightAnswer: 2
+    },
+
+    {
+      questionText: 'Как тебя зовут?',
+      answers: ['Матильда', 'Настя', 'Пётр', 'Зульфия', 'Ибрагим вам о чем-нибудь говорит?'],
+      rightAnswer: 1
     },
   ];
 
   var questionNumber = 0;
+  var result = 0;
 
   showQuestion = function(number) {
     $('.text').text(questions[number].questionText);
     $('.answers').empty();
 
     for (var i = 0; i < questions[number].answers.length; i++) {
-      $('.answers').append('<li><input type="radio" name="answer" id="answer-' + i + '"><label for="answer-' + i + '"></label></li>');
+      $('.answers').append('<li><input type="radio" name="answer" id="a' + i + '" value="' + i + '"><label for="a' + i + '"></label></li>');
       $('.answers').children('li').last().find('label').text(questions[number].answers[i]);
     };
   };
@@ -26,11 +35,16 @@ $(document).ready(function() {
   showQuestion(questionNumber);
 
   $('.next').on('click', function() {
-    questionNumber += 1;
+    if ($("input[name='answer']:checked").val() == questions[questionNumber].rightAnswer) {
+      result++;
+    }
+
+    questionNumber++;
+
     if (questionNumber < questions.length)
       showQuestion(questionNumber);
     else {
-      alert('That\'s it!');
+      alert('Результат: ' + result + '/' + questions.length);
     }
   });
 });
