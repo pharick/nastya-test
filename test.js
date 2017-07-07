e97d4f2138819372b14fe27c1697a4fef1a81b6a
Артём <phar@localhost.localdomain>
2017-07-07 09:02:17 +0300

    Some js refactoring

--- test.js
+++ test.js
@@ -1,34 +1,35 @@
+var questions = [
+  {
+    questionText: 'Сколько раз в\u00A0день ты\u00A0говоришь \u00ABмяу\u00BB?',
+    answers: ['Ни\u00A0одного', 'Всего один', 'Максимум два-три', 'Около 100 раз'],
+    rightAnswer: 3
+  },
+
+  {
+    questionText: 'Что ты\u00A0делаешь, когда видишь кота?',
+    answers: ['Прохожу мимо', 'Гавкаю', 'Кричу \u00ABкися!\u00BB и\u00A0тискаю его', 'Начинаю чесаться'],
+    rightAnswer: 2
+  },
+
+  {
+    questionText: 'Как тебя зовут?',
+    answers: ['Матильда', 'Настя', 'Пётр', 'Зульфия', 'Ибрагим вам о чем-нибудь говорит?'],
+    rightAnswer: 1
+  },
+
+  {
+    questionText: 'Что ты хочешь сейчас?',
+    answers: ['Делать уроки', 'Смотреть Newsroom', 'Мыть посуду', 'Жамкай мою ногу!'],
+    rightAnswer: 3
+  },
+];
+
 $(document).ready(function() {
-  var questions = [
-    {
-      questionText: 'Сколько раз в\u00A0день ты\u00A0говоришь \u00ABмяу\u00BB?',
-      answers: ['Ни\u00A0одного', 'Всего один', 'Максимум два-три', 'Около 100 раз'],
-      rightAnswer: 3
-    },
-
-    {
-      questionText: 'Что ты\u00A0делаешь, когда видишь кота?',
-      answers: ['Прохожу мимо', 'Гавкаю', 'Кричу \u00ABкися!\u00BB и\u00A0тискаю его', 'Начинаю чесаться'],
-      rightAnswer: 2
-    },
-
-    {
-      questionText: 'Как тебя зовут?',
-      answers: ['Матильда', 'Настя', 'Пётр', 'Зульфия', 'Ибрагим вам о чем-нибудь говорит?'],
-      rightAnswer: 1
-    },
-
-    {
-      questionText: 'Что ты хочешь сейчас?',
-      answers: ['Делать уроки', 'Смотреть Newsroom', 'Мыть посуду', 'Жамкай мою ногу!'],
-      rightAnswer: 3
-    },
-  ];
-
   var questionNumber = 0;
-  var result = 0;
+  sessionStorage['questionAmount'] = questions.length;
+  sessionStorage['result'] = 0;
 
-  showQuestion = function(number) {
+  function showQuestion(number) {
     $('.text').text(questions[number].questionText);
     $('.answers').empty();
 
@@ -37,29 +38,31 @@ $(document).ready(function() {
       $('.answers').children('li').last().find('label').text(questions[number].answers[i]);
     };
 
-    if (questionNumber == questions.length - 1)
-      $('.next').text('Узнать результат');
-
-    $('.next').css('visibility', 'hidden');
+    $('.button').hide();
 
     $("input[name='answer']").on('click', function() {
-      $('.next').css('visibility', 'visible');
+      if (questionNumber == questions.length - 1)
+        $('#finish').show();
+      else
+        $('#next').show();
     });
-  };
+  }
+
+  function getAnswer() {
+    if ($("input[name='answer']:checked").val() == questions[questionNumber].rightAnswer)
+      sessionStorage['result']++;
+  }
 
   showQuestion(questionNumber);
 
-  $('.next').on('click', function() {
-    if ($("input[name='answer']:checked").val() == questions[questionNumber].rightAnswer)
-      result++;
-
+  $('#next').on('click', function() {
+    getAnswer();
     questionNumber++;
+    showQuestion(questionNumber);
+  });
 
-    if (questionNumber < questions.length)
-      showQuestion(questionNumber);
-    else {
-      sessionStorage['result'] = Math.round(result / questions.length * 100);
-      window.location.href = "result.html";
-    }
+  $('#finish').on('click', function() {
+    getAnswer();
+    window.location.href = 'result.html';
   });
 });
